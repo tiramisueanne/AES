779a288ed580e6c7eac50e01e9adcc5cedef8072
Drothknar <sueanne@cs.utexas.edu>
2018-04-17 18:08:40 -0500

    Added a constant NB for clarity

--- aes.h
+++ aes.h
@@ -7,6 +7,9 @@ using namespace std;
 typedef unsigned char uint_8t;
 typedef unsigned int uint_32t;
 
+//this is the number of words in each round key
+const int NB =4;
+
 const uint8_t S_TABLE[256] = 
     {
         0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
@@ -98,13 +101,7 @@ private:
     //needs to be updated according to 
     //the size of the key
     uint8_t state_[4][4];
-
-    //to be updated in every round
-    uint32_t key_schedule[4];
-
-    //this will fill up our key schedule
-    void expand_key(const vector<uint_8t>& _key);
-
+    
     //Round key is added to the state
     void add_round_key(uint32_t _word);
     
