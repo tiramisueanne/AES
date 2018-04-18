053b8fb8e2dee8ba902120ba4d8de79795dabe91
bzsinger <benjamin.z.singer@gmail.com>
2018-04-17 21:11:41 -0500

    Implement get_num_rounds

--- aes.cc
+++ aes.cc
@@ -6,6 +6,10 @@
 #include "aes.h"
 using namespace std;
 
+//key size is either 128b or 256b
+//  divided into 8b chunks (in the vector)
+//  size of the vector is either 16b or 32b
+//  thus, Nk - the key length in words - is either 4 (16b / 4) or 8 (32b / 4)
 KeyMaster::KeyMaster(const vector<uint_8t>& _key) : key_size_Nk_(_key.size()/4) {
 
 }
@@ -19,13 +23,20 @@ uint32_t KeyMaster::rotate_word(uint32_t _word) {
     return 42;
 }
 
+uint8_t KeyMaster::get_num_rounds() {
+    switch(key_size_Nk_) {
+      case 4: return 10;
+      case 8: return 14;
+    }
+}
+
 AES::AES(const vector<uint_8t>& _key): master_(_key){
 }
 
 void AES::encrypt_this(string _plaintext) {
-    //do the for look that is appropriate for the 
+    //do the for look that is appropriate for the
     //key length
-    
+
 }
 
 void AES::add_round_key(uint32_t _word) {
@@ -35,7 +46,7 @@ void AES::add_round_key(uint32_t _word) {
 void AES::mix_columns() {
     //given in whitepaper
     int mult_by_mat[4][4] = { {2, 3, 1, 1}, {1, 2, 3, 1}, {1, 1, 2, 3}, {3, 1, 1, 2,}};
-    //for every column of state_ 
+    //for every column of state_
     for(int i = 0; i < 4; i++) {
         uint8_t new_col[4];
         //for every row value, we have to compute the matrix mult
