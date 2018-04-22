2f74385f1b4594f60aa6ce54cf038e22a0d1feb3
Drothknar <sueanne@cs.utexas.edu>
2018-04-21 21:26:09 -0500

    Compiles, and now our encryption is up to specifications

--- encrypt.cc
+++ encrypt.cc
@@ -8,15 +8,27 @@
 
 uint8_t ascii_to_byte(char c1, char c2) {
     //let's try out some behavior
-    const char c_style[2] = {line[i], line[i+1]};
+    const char c_style[2] = {c1, c2};
     uint8_t byte_;
     byte_ = strtol(c_style, nullptr, 16);
     return byte_;
 }
 
-string byte_to_ascii(uint8_t byte) {
-    
-};
+vector<uint8_t> byte_vector(string line) {
+    vector<uint8_t> bytes;
+    for(int i = 0; i <= line.size() - 2; i+=2) {
+        uint8_t byte_ = ascii_to_byte(line[i], line[i+1]);
+        bytes.emplace_back(byte_); 
+    }
+    return bytes;
+}
+
+void bytes_to_output(const vector<uint8_t> &bytes, ostream& oss) {
+    for(int i = 0; i < bytes.size(); i++) {
+        uint8_t byte = bytes[i];
+        oss << hex << byte; 
+    }
+}
 
 using namespace std;
 int main() {
@@ -27,16 +39,13 @@ int main() {
     if(key_file.is_open()) {
         while(getline(key_file, line)) {
             cout << "Our super secret key is " << line << endl;
-            //this is a janky line, which is a symptom of weird ostringstream behavior
-            for(int i = 0; i <= line.size() - 2; i+=2) {
-                uint8_t byte_ = ascii_to_byte(line[i], line[i+1]);
-                bytes.emplace_back(byte_); 
-            }
+            bytes = byte_vector(line);
         }
     }
     AES machine(bytes);
     cout << "Input text to encrypt: " << endl;
     string textfile;
     cin >> textfile;
-    machine.encrypt_this(textfile);    
+    vector<uint8_t> text_ = byte_vector(textfile);
+    bytes_to_output(machine.encrypt_this(text_), cout);
 }
