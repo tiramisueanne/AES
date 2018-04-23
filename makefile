edb9f62179b082b45b8bf0dd99196da53dd52b00
akashkw <akashkw@gmail.com>
2018-04-23 04:39:38 -0500

    Unit tests for string_to_byte_vector

--- makefile
+++ makefile
@@ -9,11 +9,14 @@ EXEC=aes
 all: $(BASE)
 	g++ -std=c++14 $(BASE) -o $(EXEC)
 
-# type in "test" to run all unit tests
+# type in "make test" to run all unit tests
 test: $(TESTING)
 	g++ -std=c++14 $(TESTING) -o test -lgtest -lgtest_main -pthread
 	./test
 
-#type in "clean" to get rid of all executables
+# type in "clean" to get rid of all executables
 clean:
 	rm -f $(EXEC) test
+
+# type "make runtest" to run all unit tests and clean up
+runtest: test clean
--- test.cc
+++ test.cc
@@ -1,8 +1,22 @@
 #include <vector>
-
+#include <string>
+#include <iostream>
 #include "gtest/gtest.h"
 #include "aes.h"
 
 using namespace std;
 
-TEST(KeyMasterTest, PlaceholderTest) { EXPECT_EQ(1, 1); }
+TEST(StringToByteVector, CharCompare) {
+  string sample = string("Thats my Kung Fu");
+  vector<uint8_t> from_string = string_to_byte_vector(sample);
+  for(int i = 0; i<from_string.size(); i++){
+    ASSERT_EQ(from_string[i], sample[i]);
+  }
+}
+
+TEST(StringToByteVector, CStrCompare) {
+  char const* sample = "Thats my Kung Fu";
+  vector<uint8_t> from_string = string_to_byte_vector(string(sample));
+  ASSERT_STREQ((char const*)from_string.data(), sample);
+}
+
