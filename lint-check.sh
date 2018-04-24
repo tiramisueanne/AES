ddfbd2c33f597650d17a1a9a0f233f3d0822067d
bzsinger <benjamin.z.singer@gmail.com>
2018-04-24 14:03:36 -0500

    Fixed lint-check script

--- lint-check.sh
+++ lint-check.sh
@@ -3,14 +3,18 @@
 cpplint *.cc
 cpplint *.h
 
-{ cc_diff=$(cpplint *.cc 2>&1 1>&3-) ;} 3>&1
-{ h_diff=$(cpplint *.h 2>&1 1>&3-) ;} 3>&1
+cc_diff="$(cpplint --quiet *.cc 2>&1)"
+h_diff="$(cpplint --quiet *.h 2>&1)"
+# { cc_diff=$(cpplint *.cc 2>&1 1>&3-) ;} 3>&1
+# { h_diff=$(cpplint *.h 2>&1 1>&3-) ;} 3>&1
 
 if [ "$cc_diff" == "" ] && [ "$h_diff" == "" ]
 then
+  echo
+  echo "Code linted properly."
   exit 0
 fi
 
-echo 
+echo
 echo "Code is not linted. Please lint and commit."
 exit 1
